b6b0a766c9994efdcbe5acd8731d85d152026d31
seokhyun0220 <gyouj123@naver.com>
2021-04-27 18:18:08 +0900

    2021/04/27 교재 실습 추가

---
+++ Ch02/p39.py
@@ -0,0 +1,15 @@
+num1 = 100 # 피연산자1
+num2 = 20  # 피연산자2
+
+add = num1 + num2  # 덧셈
+print('add=', add)
+sub = num1 - num2  # 뺄셈
+print(sub)
+mul = num1 * num2  # 곱셈
+print(mul)
+div = num1 / num2  # 나눗셈
+print(div)
+div2 = num1 % num2 # 나머지 계산
+print(div2)
+square = num1**2   # 제곱계산
+print(square)
---
+++ Ch02/p48.py
@@ -0,0 +1,11 @@
+# 문자열 유형
+oneLine = "this is one line string"
+print(oneLine)
+
+multiLine = """this is
+multi line
+striong"""
+print(multiLine)
+
+multiLine2 = "this is\nmulti line\nstring"
+print(multiLine2)
